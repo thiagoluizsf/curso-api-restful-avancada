28ff84e16034fa9fcc1907b81582ae4777da427c
thiagoluizsf <thiagoluizsf@gmail.com>
2021-12-07 17:08:16 -0300

    atualização no projeto e criacao das entities

---
+++ src/main/java/com/thiago/pontoInteligente/api/entities/Empresa.java
@@ -0,0 +1,31 @@
+package com.thiago.pontoInteligente.api.entities;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "TB_EMPRESA")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Empresa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String razaoSocial;
+	private String cnpj;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+	private List<Funcionario> funcionarios;
+}
---
+++ src/main/java/com/thiago/pontoInteligente/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.thiago.pontoInteligente.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
