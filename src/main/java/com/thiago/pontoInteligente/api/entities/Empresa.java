a214e782c815e8d181f4b6f84b81a4ccb83bea14
thiagoluizsf <thiagoluizsf@gmail.com>
2021-12-10 14:47:18 -0300

    entidades jpa

--- src/main/java/com/thiago/pontoInteligente/api/entities/Empresa.java
+++ src/main/java/com/thiago/pontoInteligente/api/entities/Empresa.java
@@ -3,10 +3,15 @@ package com.thiago.pontoInteligente.api.entities;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -23,9 +28,32 @@ public class Empresa {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@Column(name = "razao_social")
 	private String razaoSocial;
+	@Column(name = "cnpj")
 	private String cnpj;
+	@Column(name = "dataCriacao")
 	private Date dataCriacao;
+	@Column(name = "dataAtualizacao")
 	private Date dataAtualizacao;
+	@OneToMany(mappedBy = "empresa", cascade = CascadeType.ALL)
 	private List<Funcionario> funcionarios;
+	
+	@PreUpdate
+	public void preUpdate() {
+		dataAtualizacao = new Date();
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		final Date dataAtual = new Date();
+		dataCriacao = dataAtual;
+		dataAtualizacao = dataAtual;
+	}
+
+	@Override
+	public String toString() {
+		return "Empresa [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + ", dataCriacao=" + dataCriacao
+				+ ", dataAtualizacao=" + dataAtualizacao + ", funcionarios=" + funcionarios + "]";
+	}
 }
---
+++ src/main/java/com/thiago/pontoInteligente/api/entities/Lancamento.java
@@ -0,0 +1,51 @@
+package com.thiago.pontoInteligente.api.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.thiago.pontoInteligente.api.enums.TipoEnum;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_lancamento")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Lancamento {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "data")
+	private Date data;
+	@Column(name = "descricao")
+	private String descricao;
+	@Column(name = "localizacao")
+	private String localizacao;
+	@Column(name = "data_criacao")
+	private Date dataCriacao;
+	@Column(name = "dataAtualizacao")
+	private Date dataAtualizacao;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "tipo")
+	private TipoEnum tipo;
+	@ManyToOne
+	@Column(name = "id_funcionario")
+	private Funcionario funcionario;
+	
+}
